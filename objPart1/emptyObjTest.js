c1cb0c045dd3c2db36f199bd2efe6a4fa00c45da
amanuelgezahange12 <69604107+amanuelgezahange12@users.noreply.github.com>
2020-09-10 02:28:22 -0500

    updated
    updated

---
+++ objPart1/emptyObjTest.js
@@ -0,0 +1,11 @@
+"use strict";
+/* global isEmpty assert isEmpty */
+
+describe("is object empty?", function() {
+
+    it(" yes object is empty", function() {
+      assert.equal(true, isEmpty({}));
+    });
+  
+
+  });
